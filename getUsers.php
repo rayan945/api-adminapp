04c6742d0def9e600c9ea82a3968a338ea5645ed
rayan945 <rayansainsard88@gmail.com>
2026-03-29 18:21:22 +0200

    add vercel config

--- getUsers.php
+++ getUsers.php
@@ -1,8 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Accept");
+header("Content-Type: application/json; charset=utf-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
